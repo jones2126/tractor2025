4d91f91390a7cdb49656879bb0667abdb281327a
Al Jones <aej2126@protonmail.com>
2025-04-08 17:00:59 -0400

    added serial print

--- teensy_blink/src/main.cpp
+++ teensy_blink/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13, OUTPUT);  // Onboard LED
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  Serial.println("Hello from Teensy!");
   digitalWrite(13, HIGH);
   delay(500);
   digitalWrite(13, LOW);
