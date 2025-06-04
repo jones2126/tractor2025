7bac70f8a83517d22ba9a3f98272953fd2716cb8
Al Jones <aej2126@protonmail.com>
2025-06-04 15:48:58 -0500

    changed radio addresses

--- teensynrf24tractor20250602/src/main.cpp
+++ teensynrf24tractor20250602/src/main.cpp
@@ -113,9 +113,13 @@ void setup() {
     radio.setPALevel(RF24_PA_HIGH);
     radio.setDataRate(RF24_250KBPS);
     radio.setChannel(124);
-    radio.openWritingPipe(address[1]);    // "TRACT" = robot tractor
-    radio.openReadingPipe(1, address[0]); // "RCTRL" = radio control unit
-    radio.enableAckPayload();             // THIS IS CRITICAL!
+    // radio.openWritingPipe(address[1]);    // "TRACT" = robot tractor
+    // radio.openReadingPipe(1, address[0]); // "RCTRL" = radio control unit
+
+    radio.openWritingPipe(address[0]);    // "RCTRL" = send ACKs back to control unit
+    radio.openReadingPipe(1, address[1]); // "TRACT" = listen for data sent TO tractor
+
+    radio.enableAckPayload(); 
     radio.startListening();
     radio.printDetails();
 
