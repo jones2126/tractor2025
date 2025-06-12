b29fd7526c2370847fa00d9380b8e5c0df840a42
Al Jones <aej2126@protonmail.com>
2025-06-12 12:29:40 -0500

    using setTargetCompact

--- jrkG2_range_test/src/main.cpp
+++ jrkG2_range_test/src/main.cpp
@@ -10,6 +10,12 @@ void setTarget(uint16_t target) {
   Serial3.write((target >> 5) & 0x7F); // 7 MSBs
 }
 
+void setTargetCompact(uint16_t target) {
+  if (target > 4095) target = 4095;  // Clamp range
+  Serial3.write(0xC0 + (target & 0x1F));       // Command byte with 5 LSBs
+  Serial3.write((target >> 5) & 0x7F);         // 7 MSBs
+}
+
 void setup() {
   delay(45000);  // delay to let RPi boot up so Serial will work
   Serial.begin(115200);      // Serial monitor for debugging
@@ -19,11 +25,14 @@ void setup() {
 void loop() {
   jrkTarget = 2048; // Midpoint
   Serial.println("Moving to " + String(jrkTarget));
-  setTarget(jrkTarget); delay(jrkPause); // Midpoint
+  // setTarget(jrkTarget); delay(jrkPause); // Midpoint
+  setTargetCompact(jrkTarget); delay(jrkPause); // Midpoint
   jrkTarget = 4095; // Fully Extend
   Serial.println("Moving to " + String(jrkTarget));
-  setTarget(jrkTarget); delay(jrkPause);   
+  // setTarget(jrkTarget); delay(jrkPause);  
+  setTargetCompact(jrkTarget); delay(jrkPause); 
   jrkTarget = 0; // Retract
   Serial.println("Moving to " + String(jrkTarget));
-  setTarget(jrkTarget); delay(jrkPause);   
+  // setTarget(jrkTarget); delay(jrkPause);
+  setTargetCompact(jrkTarget); delay(jrkPause); 
 }
