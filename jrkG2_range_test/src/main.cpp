c542306daf79c88cb88f6cbfd1b64b846ddb03a2
Al Jones <aej2126@protonmail.com>
2025-06-12 09:14:43 -0500

    added new test for JRK

---
+++ jrkG2_range_test/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+unsigned long jrkTarget = 0;
+void setup() {
+  delay(45000);  // delay to let RPi boot up so Serial will work
+  Serial.begin(115200);      // Serial monitor for debugging
+  Serial3.begin(9600); // Match JRK baud rate
+}
+
+void loop() {
+  jrkTarget = 2048; // Midpoint
+  Serial.println("Moving to " + String(jrkTarget));
+  setTarget(jrkTarget); delay(2000); // Midpoint
+  jrkTarget = 4095; // Fully Extend
+  Serial.println("Moving to " + String(jrkTarget));
+  setTarget(jrkTarget); delay(2000);   
+  jrkTarget = 0; // Retract
+  Serial.println("Moving to " + String(jrkTarget));
+  setTarget(jrkTarget); delay(2000);   
+}
+
+void setTarget(uint16_t target) {
+  Serial3.write(0xAA);               // Start byte
+  Serial3.write(11);                 // Device number
+  Serial3.write(0x40);               // Set Target command
+  Serial3.write(target & 0x1F);      // 5 LSBs
+  Serial3.write((target >> 5) & 0x7F); // 7 MSBs
+}
