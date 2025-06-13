3ac40bbaac0c392c7fa70c0a081e37fe8971ee0d
Al Jones <aej2126@protonmail.com>
2025-06-13 12:52:38 -0500

    added test script

--- teensyNRF24/src/main.cpp
+++ teensyNRF24/src/main.cpp
@@ -1,19 +1,43 @@
-#include <Arduino.h>
+//#include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define CE and CSN pins for Teensy
+RF24 radio(9, 10);
+
+// Must match the struct sent from the transmitter
+struct RadioData {
+  uint16_t throttle_val;     // Optional
+  uint16_t transmission_val; // What we're interested in
+  uint8_t control_mode;
+};
+
+RadioData radioData;
+
+// Must match transmitter pipe
+const byte address[6] = "1Node";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial);  // Wait for USB Serial
+
+  if (!radio.begin()) {
+    Serial.println("NRF24 initialization failed!");
+    while (1);
+  }
+
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+
+  Serial.println("NRF24 initialized and listening...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (radio.available()) {
+    radio.read(&radioData, sizeof(radioData));
+    Serial.print("transmission_val = ");
+    Serial.println(radioData.transmission_val);
+  }
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-//this is a test
