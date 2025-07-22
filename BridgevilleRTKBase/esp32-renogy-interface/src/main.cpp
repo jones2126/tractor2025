ec2a70731ab03ba5a196295069d93ab25b32dc36
Al Jones <aej2126@protonmail.com>
2025-07-22 18:19:31 -0400

    led test

--- BridgevilleRTKBase/esp32-renogy-interface/src/main.cpp
+++ BridgevilleRTKBase/esp32-renogy-interface/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
+// Common LED pins on different ESP32 boards
+int ledPins[] = {2, 5, 16, 17, 18, 19, 21, 22, 23};
+int numPins = sizeof(ledPins) / sizeof(ledPins[0]);
+
 void setup() {
   Serial.begin(115200);
-  pinMode(2, OUTPUT);  // Built-in LED
   delay(2000);  // Give serial monitor time to connect
-  Serial.println("ESP32 Started - Simple Test");
+  Serial.println("ESP32 LED Pin Test Started");
+  Serial.println("=========================");
+  Serial.println("Testing LED on different pins...");
+  
+  // Initialize all potential LED pins
+  for (int i = 0; i < numPins; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);  // Start with LEDs off
+  }
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  Serial.println("LED ON - Test Message");
-  delay(1000);
+  // Test each pin one by one
+  for (int i = 0; i < numPins; i++) {
+    Serial.print("Testing LED on GPIO ");
+    Serial.println(ledPins[i]);
+    
+    // Turn on LED
+    digitalWrite(ledPins[i], HIGH);
+    delay(1000);
+    
+    // Turn off LED  
+    digitalWrite(ledPins[i], LOW);
+    delay(500);
+  }
   
-  digitalWrite(2, LOW);
-  Serial.println("LED OFF - Test Message");  
-  delay(1000);
+  Serial.println("------------------------");
+  Serial.println("Cycle complete. Watch for blinking LED!");
+  delay(2000);
 }
