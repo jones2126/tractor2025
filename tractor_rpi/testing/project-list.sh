6ae67eca786de701c9f967a582462a2e4e00196c
Al Jones <aej2126@protonmail.com>
2026-06-14 15:35:36 -0400

    Reorganize: move test files to testing/, remove old rtcm_server_20251220.py

---
+++ tractor_rpi/testing/project-list.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "=== Teensy 4.1 (tractor) ==="
+ls /home/al/tractor2025/tractor_teensy/src/
+
+echo ""
+echo "=== Teensy 3.2 (RC handheld) ==="
+ls /home/al/tractor2025/radiocontrol_nrf24radio/src/
+
+echo ""
+echo "=== RPi Python services ==="
+ls /home/al/tractor2025/tractor_rpi/*.py
+
+echo ""
+echo "=== Bridgeville RTK base ==="
+ls /home/al/tractor2025/BridgevilleRTKBase/raspberry-pi/production/
